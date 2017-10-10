5ea9739c7b17395ffe7d0a8d05cea4fc401f2a75
Duke LeTran-work <daletran@ucdavis.edu>
2017-10-10 12:05:26 -0700

    Added information about remote push/pull

--- git-practice/gitnotes.sh
+++ git-practice/gitnotes.sh
@@ -7,28 +7,46 @@
 # git add --help
 # git config --global user.name
 
-## VISUAL
+## ASCII VISUAL
 #                             |-------------Commit---->	
 # |------------Stage Fixes---------->
-# Working Directory			Staging Area		.git directory
-
-
+# [Working Directory]			[Staging Area]		[.git directory]
+#             <---------------Checkout-----------------
+#
 
 ## BASICS and SETUP
-git --version #check if git exists
+git --version #check if git exists, version
 git initi #intiatializes the folder for git
 #rm -rf .git // this completely removes git from the the folder
 
 #set up first time ever
 git config --global user.name "Duke LeTran"
-git config --global user.email "duke.letran@gmail.com"
+git config --global user.email "daletran@ucdavis.edu"
 
 #.gitignore
 touch .gitignore
 subl .gitignore #add things like *.csv to ignorefile
 
-#Simple commands
+#connecting with remote repositories
+git clone <url> <where-to-clone> #must be empty director or new
+
+#Staging commands
 git status #checks status
+git add -A #adds all
+git add <file-name>
+git reset #moves all files from staging area to wd
+
+#commiment
+git commit -m "Initial Commit"
+git diff 
+
+## REMOTE
+# note: origin is the remote repository, master is the "branch"
+git pull origin master
+git push origin master
+
+
+
 
 
 
