8c78f5487954d14179bf24d982a5f3d78ad2339a
Duke-LeTran <duke.letran@gmail.com>
2019-11-27 09:25:58 -0800

    data camp practice file

---
+++ main_datacamp.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 26 13:50:12 2019
+
+@author: dletran
+"""
+#%%
+from sklearn import datasets
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+plot.style.use('ggplot')
+#%%
+df_iris = datasets.load_iris()
